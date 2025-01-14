c92811aff084445219acff7e8a58381bc7ae488a
Walassi Silva <walassi.b@gmail.com>
2025-01-13 23:20:22 -0300

    feat: add hidden message

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const helmet = document.querySelector('.helmet');
+const star = document.querySelector('.s6');
 const message = document.querySelector('.message');
+const secret = document.querySelector('.secret');
 
 helmet.addEventListener('click', () => {
-  message.style.display = 'block';
+  message.style.display = 'block';  
+});
+star.addEventListener('click', () => {
+  secret.style.display = 'block';  
+  console.log('secret');
   
 });
