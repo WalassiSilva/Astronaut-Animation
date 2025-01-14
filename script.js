9ba8936bb9e16613d77dca1e33ee829bfba8db75
Walassi Silva <walassi.b@gmail.com>
2025-01-13 22:55:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const helmet = document.querySelector('.helmet');
+const message = document.querySelector('.message');
+
+helmet.addEventListener('click', () => {
+  message.style.display = 'block';
+  
+});
