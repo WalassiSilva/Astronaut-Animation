f2723e320613421cb7df1f6ee7934dbe1fdc8b6f
Walassi Silva <walassi.b@gmail.com>
2025-01-15 19:59:46 -0300

    feat: add more secret messages

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const helmet = document.querySelector('.helmet');
 const star = document.querySelector('.s6');
 const message = document.querySelector('.message');
+const secret1 = document.querySelectorAll('.secret');
 const secret = document.querySelector('.secret');
 const btn = document.querySelector('.btn');
 const blackout = document.querySelector('.blackout');
+const star1 = document.querySelector('.s1');
 
 helmet.addEventListener('click', () => {
   message.style.display = 'block';  
@@ -13,6 +15,14 @@ star.addEventListener('click', () => {
   console.log('secret');
   setTimeout(() => {
     secret.style.display = 'none';
+  }, 1000);  
+});
+
+star1.addEventListener('click', () => {
+  secret1[1].style.display = 'block';  
+  console.log('secret');
+  setTimeout(() => {
+    secret1[1].style.display = 'none';
   }, 1000);  
 });
 
